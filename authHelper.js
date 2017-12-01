ee6653d4baea667e26b613c9cf5c19c76acb536f
Cecy Correa <cecy.correa@returnpath.com>
2017-12-01 09:11:30 -0600

    initial commit

---
+++ authHelper.js
@@ -0,0 +1,50 @@
+var credentials = {
+  client: {
+    id: 'f2a5c9da-419d-479a-8481-15096eb99a48',
+    secret: 'jbqHUZY44_=ufzcRQJ876]*',
+  },
+  auth: {
+    tokenHost: 'https://login.microsoftonline.com',
+    authorizePath: 'common/oauth2/v2.0/authorize',
+    tokenPath: 'common/oauth2/v2.0/token'
+  }
+};
+var oauth2 = require('simple-oauth2').create(credentials);
+
+var redirectUri = 'http://localhost:8000/authorize';
+
+// The scopes the app requires
+var scopes = [ 'openid',
+               'User.Read',
+               'Mail.Read' ];
+
+function getAuthUrl() {
+  var returnVal = oauth2.authorizationCode.authorizeURL({
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  });
+  console.log('Generated auth url: ' + returnVal);
+  return returnVal;
+}
+
+exports.getAuthUrl = getAuthUrl;
+
+function getTokenFromCode(auth_code, callback, response) {
+  var token;
+  oauth2.authorizationCode.getToken({
+    code: auth_code,
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  }, function (error, result) {
+    if (error) {
+      console.log('Access token error: ', error.message);
+      callback(response, error, null);
+    } else {
+      token = oauth2.accessToken.create(result);
+      console.log('Token created: ', token.token);
+      callback(response, null, token);
+    }
+  });
+}
+
+exports.getTokenFromCode = getTokenFromCode;
---
+++ index.js
@@ -0,0 +1,71 @@
+var server = require('./server');
+var router = require('./router');
+var authHelper = require('./authHelper');
+var microsoftGraph = require("@microsoft/microsoft-graph-client");
+
+var handle = {};
+handle['/'] = home;
+handle['/authorize'] = authorize;
+
+server.start(router.route, handle);
+
+function home(response, request) {
+  console.log('Request handler \'home\' was called.');
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<p>Please <a href="' + authHelper.getAuthUrl() + '">sign in</a> with your Office 365 or Outlook.com account.</p>');
+  response.end();
+}
+
+var url = require('url');
+function authorize(response, request) {
+  console.log('Request handler \'authorize\' was called.');
+
+  // The authorization code is passed as a query parameter
+  var url_parts = url.parse(request.url, true);
+  var code = url_parts.query.code;
+  console.log('Code: ' + code);
+  authHelper.getTokenFromCode(code, tokenReceived, response);
+}
+
+function getUserEmail(token, callback) {
+  // Create a Graph client
+  var client = microsoftGraph.Client.init({
+    authProvider: (done) => {
+      // Just return the token
+      done(null, token);
+    }
+  });
+
+  // Get the Graph /Me endpoint to get user email address
+  client
+    .api('/me')
+    .get((err, res) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, res.mail);
+      }
+    });
+}
+
+function tokenReceived(response, error, token) {
+  if (error) {
+    console.log('Access token error: ', error.message);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<p>ERROR: ' + error + '</p>');
+    response.end();
+  } else {
+    getUserEmail(token.token.access_token, function(error, email) {
+      if (error) {
+        console.log('getUserEmail returned an error: ' + error);
+        response.write('<p>ERROR: ' + error + '</p>');
+        response.end();
+      } else if (email) {
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('<p>Email: ' + email + '</p>');
+        response.write('<p>Access token: ' + token.token.access_token + '</p>');
+        response.end();
+      }
+    });
+  }
+}
